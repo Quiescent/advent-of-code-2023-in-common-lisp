669f64a0582789f71811285fe671eb9372b44987
Edward John Steere <edward.steere@gmail.com>
2023-12-09 07:19:26 +0200

    Solve day 9

--- src/day-9.lisp
+++ src/day-9.lisp
@@ -19,13 +19,52 @@
                (bind ((line (read-line f nil nil)))
                  (if (null line)
                      acc
-                     ))))
+                     (recur (cons (-> (format nil "(~a)" line) read-from-string)
+                                  acc))))))
       (recur nil))))
+
+(defun diffs (xs)
+  (when (and xs (cdr xs))
+    (cons (- (cadr xs)
+             (car xs))
+          (diffs (cdr xs)))))
+
+(defun recursive-diffs (xs)
+  (bind ((next-diffs (diffs xs)))
+    (when (not (every (lambda (x) (= x 0)) next-diffs))
+      (cons next-diffs
+            (recursive-diffs next-diffs)))))
+
+(defun next-number (xs)
+  (bind ((ds (nreverse (recursive-diffs xs))))
+    (labels ((recur (ys prev-last)
+               (if (null ys)
+                   prev-last
+                   (bind ((next-last (last (car ys))))
+                     (recur (cdr ys)
+                            (+ prev-last
+                               next-last))))))
+      (+ (recur (cdr ds)
+                (last (car ds)))
+         (last xs)))))
 
 (defun part-1 ()
   (bind ((problem (read-problem)))
-    ))
+    (apply #'+ (mapcar #'next-number problem))))
+
+(defun prev-number (xs)
+  (bind ((ds (nreverse (recursive-diffs xs))))
+    (labels ((recur (ys prev-last)
+               (if (null ys)
+                   prev-last
+                   (bind ((next-last (car (car ys))))
+                     (recur (cdr ys)
+                            (- next-last
+                               prev-last))))))
+      (- (car xs)
+         (recur (cdr ds)
+                (car (car ds)))))))
 
 (defun part-2 ()
   (bind ((problem (read-problem)))
-    ))
+    (apply #'+ (mapcar #'prev-number problem))))
