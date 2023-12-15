9b184b32085ebb31e97d1e25b26090f22c9e3d7c
Edward John Steere <edward.steere@gmail.com>
2023-12-15 07:31:13 +0200

    Solve day 15

--- src/day-15.lisp
+++ src/day-15.lisp
@@ -15,17 +15,54 @@
 
 (defun read-problem ()
   (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2023-in-common-lisp "src/day-15.in"))
-    (labels ((recur (acc)
-               (bind ((line (read-line f nil nil)))
-                 (if (null line)
-                     acc
-                     ))))
-      (recur nil))))
+    (bind ((line (read-line f nil nil)))
+      (split "," line))))
+
+(defun hash (step)
+  (bind ((result 0))
+    (iter
+      (for char in-string step)
+      (incf result (char-code char))
+      (setf result (* result 17))
+      (setf result (mod result 256))
+      (finally (return result)))))
 
 (defun part-1 ()
   (bind ((problem (read-problem)))
-    ))
+    (iter
+      (for step in problem)
+      (summing (hash step)))))
 
 (defun part-2 ()
-  (bind ((problem (read-problem)))
-    ))
+  (bind ((problem (read-problem))
+         (boxes (make-hash-table :test #'equal)))
+    (iter
+      (for instruction in problem)
+      (match instruction
+        ((ppcre "([a-zA-Z]+)=([0-9]+)"
+                label
+                (read lense))
+         (bind ((box-idx (hash label))
+                (lenses (gethash box-idx boxes)))
+           (if (member label lenses :key #'car :test #'equal)
+               (setf (gethash box-idx boxes)
+                     (mapcar (lambda (label-lense)
+                               (if (string-equal (car label-lense) label)
+                                   (cons label lense)
+                                   label-lense))
+                             lenses))
+               (setf (gethash box-idx boxes)
+                     (nconc lenses (list (cons label lense)))))))
+        ((ppcre "([a-zA-Z]+)-"
+                label)
+         (bind ((box-idx (hash label))
+                (lenses (gethash box-idx boxes)))
+           (setf (gethash box-idx boxes)
+                 (remove label lenses :key #'car :test #'equal))))))
+    (iter
+      (for (box-idx lenses) in-hashtable boxes)
+      (summing
+       (iter
+         (for i from 1)
+         (for (label . lense) in lenses)
+         (summing (* (1+ box-idx) i lense)))))))
