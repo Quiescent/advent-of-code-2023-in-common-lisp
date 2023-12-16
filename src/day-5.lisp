55115a1b67b7af4ddf8c15119c8e5172fccd56d4
Edward John Steere <edward.steere@gmail.com>
2023-12-16 12:38:31 +0200

    Revisit day 5 and implement range intersection

--- src/day-5.lisp
+++ src/day-5.lisp
@@ -174,3 +174,139 @@
                  )))
       (read-problem))))
 
+;; Alternative solution
+
+(defstruct range
+  (start 0 :type fixnum)
+  (end 0 :type fixnum)
+  (sub 0 :type fixnum))
+
+(defun read-problem ()
+  (with-open-file (f (asdf:system-relative-pathname :advent-of-code-2023-in-common-lisp "src/day-5.in"))
+    (labels ((recur (acc current-mapping)
+               (bind ((line (read-line f nil nil)))
+                 (cond
+                   ((null line) (nreverse (cons (nreverse current-mapping) acc)))
+                   ((string-equal line "") (recur (cons (nreverse current-mapping) acc) nil))
+                   (t (match line
+                        ((ppcre "(-?[0-9]+) (-?[0-9]+) (-?[0-9]+)"
+                                (read dest-start)
+                                (read source-start)
+                                (read range-length))
+                         (recur acc (cons (make-range :start source-start
+                                                      :end (+ source-start range-length)
+                                                      :sub (- source-start dest-start))
+                                          current-mapping)))
+                        (_ (recur acc current-mapping))))))))
+      (cons (->> (read-line f nil nil) (split " ") cdr (mapcar #'read-from-string))
+            (progn (read-line f nil nil)
+                   (recur nil nil))))))
+
+(defmethod range-contains ((r range) seed)
+  (with-slots (start end) r
+    (and (>= seed start)
+         (<= seed end))))
+
+(defmethod range-transform ((r range) seed)
+  (with-slots (sub) r
+    (- seed sub)))
+
+(defun part-1-alt ()
+  (bind (((seeds . categories) (read-problem)))
+    (iter
+      (for seed in seeds)
+      (minimize
+       (iter
+         (for category in categories)
+         (for range = (find-if (lambda (range) (range-contains range seed)) category))
+         (when range
+           (setf seed (range-transform range seed)))
+         (finally (return seed)))))))
+
+(defun seeds-to-ranges (seeds)
+  (when seeds
+    (cons (make-range :start (car seeds)
+                      :end (+ (car seeds) (cadr seeds))
+                      :sub 0)
+          (seeds-to-ranges (cddr seeds)))))
+
+(defmethod range-intersection ((this range) (other range))
+  (bind ((this-start (range-start this))
+         (this-end (range-end this))
+         (other-start (range-start other))
+         (other-end (range-end other))
+         (other-sub (range-sub other)))
+    (cond
+
+      ;; Contained
+      ((and (>= this-start other-start)
+            (<= this-end other-end))
+       (cons nil
+             (make-range :start (- this-start other-sub)
+                         :end (- this-end other-sub)
+                         :sub other-sub)))
+
+      ;; Overlaps on either side
+      ((and (< this-start other-start)
+            (> this-end other-end))
+       (cons (list (make-range :start this-start
+                               :end (1- other-start)
+                               :sub other-sub)
+                   (make-range :start (1+ other-start)
+                               :end this-end
+                               :sub other-sub))
+             (make-range :start (- other-start other-sub)
+                         :end (- other-end other-sub)
+                         :sub other-sub)))
+
+      ;; Overlaps at start
+      ((and (< this-start other-start)
+            (>= this-end other-start)
+            (<= this-end other-end))
+       (cons (list (make-range :start this-start
+                               :end (1- other-start)
+                               :sub other-sub))
+             (make-range :start (- other-start other-sub)
+                         :end (- this-end other-sub)
+                         :sub other-sub)))
+
+      ;; Overlaps at end
+      ((and (<= this-start other-end)
+            (>= this-start other-start)
+            (> this-end other-end))
+       (cons (list (make-range :start (+ other-end 1)
+                               :end this-end
+                               :sub other-sub))
+             (make-range :start (- this-start other-sub)
+                         :end (- other-end other-sub)
+                         :sub other-sub)))
+
+      ;; No intersection
+      (t (cons (list this) nil)))))
+
+(defun intersect-all (range category)
+  (if (null category)
+      (list range)
+      (bind (((outside . intersected) (range-intersection range (car category))))
+        (bind ((rest (reduce (lambda (acc next-out)
+                               (nconc acc (intersect-all next-out (cdr category))))
+                             outside
+                             :initial-value nil)))
+         (if intersected (cons intersected rest) rest)))))
+
+(defun transform-ranges (ranges categories)
+  (if (null categories)
+      ranges
+      (bind (((category . rest) categories)
+             (new-ranges (reduce (lambda (acc range)
+                                   (nconc acc (intersect-all range category)))
+                                 ranges
+                                 :initial-value nil)))
+        (transform-ranges new-ranges rest))))
+
+(defun part-2-alt ()
+  (bind (((seeds . categories) (read-problem))
+         (in-ranges (seeds-to-ranges seeds)))
+    (->> (transform-ranges in-ranges categories)
+      (mapcar #'range-start)
+      (apply #'min))))
