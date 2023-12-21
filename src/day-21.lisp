036082c7f3bd7793b91e99dacb2e1da06693e4ce
Edward Steere <edward.steere@gmail.com>
2023-12-21 13:56:46 +0200

    wip

--- src/day-21.lisp
+++ src/day-21.lisp
@@ -91,9 +91,9 @@
       (while (not (empty? queue)))
       (for current = (first queue))
       ;; (format t "current: ~a~%" current)
+      (setf queue (less-first queue))
       (when (>= (gethash current seen) max-distance)
         (next-iteration))
-      (setf queue (less-first queue))
       (iter
         (for direction in directions)
         (for next = (+ current direction))
@@ -105,57 +105,12 @@
           (setf queue (with-last queue next)))))
     seen))
 
-(defun count-separate (seen)
+(defun count-reachable (seen)
   (iter
     (for (key value) in-hashtable seen)
-    (counting (= (mod value 2) 0) into lights)
-    (counting (/= (mod value 2) 0) into darks)
-    (finally (return (cons lights darks)))))
+    (counting (= (mod value 2) 1))))
 
-(defun invert-colour (colour)
-  (if (eq colour 'light) 'dark 'light))
-
-(defun meta-bfs (grid initial-remaining)
-  (bind ((queue (empty-seq))
-         (seen (make-hash-table :test #'equal))
-         (start (starting-point grid))
-         (start-x (realpart start))
-         (start-y (imagpart start))
-         (left-entry (complex 0 start-y))
-         (right-entry (complex (1- (length (aref grid 0))) start-y))
-         (top-entry (complex start-x 0))
-         (bottom-entry (complex start-x (1- (length grid))))
-         (directions (list (cons right left-entry)
-                           (cons down top-entry)
-                           (cons left right-entry)
-                           (cons up bottom-entry)))
-         (start-bfs (unlocked-bfs grid start (* 130 130)))
-         (steps-to-fill (+ (floor (1- (length grid)) 2) (1- (length grid))))
-         ((light-size . dark-size) (count-separate start-bfs)))
-    (format t "steps-to-fill: ~a~%" steps-to-fill)
-    (setq queue (with-last queue (list #c(0 0) (starting-point grid) initial-remaining 'light)))
-    (iter
-      (while (not (empty? queue)))
-      (for (meta-coord entry-point remaining colour) = (first queue))
-      (format t "meta-coord: ~a~%" meta-coord)
-      (setf queue (less-first queue))
-      (if (>= remaining steps-to-fill)
-          (progn
-            (setf (gethash meta-coord seen) (if (eq colour 'light) light-size dark-size))
-            (iter
-              (for (direction . next-start) in directions)
-              (for next-meta-coord = (+ direction meta-coord))
-              (when (not (gethash next-meta-coord seen))
-                (setf queue (with-last queue (list next-meta-coord
-                                                   next-start
-                                                   (- remaining steps-to-fill)
-                                                   (invert-colour colour)))))))
-          (progn
-            (print "HERE")
-            (setf (gethash meta-coord seen)
-                  (unlocked-bfs grid entry-point remaining)))))))
-
-(defun count-reachable (seen)
+(defun count-reachable-odds (seen)
   (iter
     (for (key value) in-hashtable seen)
     (counting (= (mod value 2) 0))))
@@ -167,10 +122,21 @@
       (setf (gethash key-1 new-seen) value-1))
     (iter
       (for (key-2 value-2) in-hashtable seen-2)
-      (setf (gethash key-2 new-seen) value-2))))
+      (setf (gethash key-2 new-seen) value-2))
+    new-seen))
+
+(defun sum-odds (m)
+  (iter
+    (for i from 1 to m by 2)
+    (summing i)))
+
+(defun diamond-perimeter (tri-length)
+  (if (= 1 tri-length)
+      1
+      (* 4 (1- tri-length))))
 
 (defun totals (grid initial-remaining)
-  (bind ((start (print (starting-point grid)))
+  (bind ((start (starting-point grid))
          (start-x (realpart start))
          (start-y (imagpart start))
          (left-entry (complex 0 start-y))
@@ -178,44 +144,84 @@
          (top-entry (complex start-x 0))
          (bottom-entry (complex start-x (1- (length grid))))
          (start-bfs (unlocked-bfs grid start (* (length grid) (length grid))))
-         ;; (steps-remaining (mod (- initial-remaining start-x) (length grid)))
-         ;; (seen-entering-bottom (unlocked-bfs grid
-         ;;                                     bottom-entry
-         ;;                                     steps-remaining))
-         ;; (seen-entering-left (unlocked-bfs grid
-         ;;                                   left-entry
-         ;;                                   steps-remaining))
-         ;; (seen-entering-right (unlocked-bfs grid
-         ;;                                    right-entry
-         ;;                                    steps-remaining))
-         ;; (seen-entering-top (unlocked-bfs grid
-         ;;                                  top-entry
-         ;;                                  steps-remaining))
-         ;; (seen-bottom (count-reachable seen-entering-bottom))
-         ;; (seen-left (count-reachable seen-entering-left))
-         ;; (seen-right (count-reachable seen-entering-right))
-         ;; (seen-top (count-reachable seen-entering-top))
-         ;; (seen-top-right-edge (count-reachable (merge-seen seen-entering-bottom
-         ;;                                                   seen-entering-left)))
-         ;; (seen-bottom-right-edge (count-reachable (merge-seen seen-entering-top
-         ;;                                                      seen-entering-left)))
-         ;; (seen-top-left-edge (count-reachable (merge-seen seen-entering-right
-         ;;                                                  seen-entering-bottom)))
-         ;; (seen-bottom-left-edge (count-reachable (merge-seen seen-entering-right
-         ;;                                                     seen-entering-top)))
-         (all-seen-reachable (count-reachable start-bfs))
+         (steps-remaining (length grid))
+         (seen-entering-bottom (unlocked-bfs grid
+                                             bottom-entry
+                                             steps-remaining))
+         (seen-entering-left (unlocked-bfs grid
+                                           left-entry
+                                           steps-remaining))
+         (seen-entering-right (unlocked-bfs grid
+                                            right-entry
+                                            steps-remaining))
+         (seen-entering-top (unlocked-bfs grid
+                                          top-entry
+                                          steps-remaining))
+         (seen-bottom (count-reachable-odds seen-entering-bottom))
+         (seen-left (count-reachable-odds seen-entering-left))
+         (seen-right (count-reachable-odds seen-entering-right))
+         (seen-top (count-reachable-odds seen-entering-top))
+         (seen-top-right-edge (count-reachable-odds (merge-seen seen-entering-bottom
+                                                           seen-entering-left)))
+         (seen-bottom-right-edge (count-reachable-odds (merge-seen seen-entering-top
+                                                              seen-entering-left)))
+         (seen-top-left-edge (count-reachable-odds (merge-seen seen-entering-right
+                                                          seen-entering-bottom)))
+         (seen-bottom-left-edge (count-reachable-odds (merge-seen seen-entering-right
+                                                             seen-entering-top)))
+         (all-seen-reachable-evens (count-reachable start-bfs))
+         (all-seen-reachable-odds (count-reachable-odds start-bfs))
          (tri-length (floor initial-remaining (length grid)))
-         (tri-area (* (floor tri-length 2) tri-length))
-         (interior-size (* tri-area 4)))
+         (side-to-side (1+ (* 2 (1- tri-length))))
+         (interior-size (+ side-to-side (* 2 (sum-odds (- side-to-side 2)))))
+         (interior-size-evens (iter
+                                (for i from 2 below tri-length by 2)
+                                ;; (format t "i: ~a~%" (diamond-perimeter i))
+                                (summing (diamond-perimeter i))))
+         (interior-size-odds (iter
+                               (for i from 1 below tri-length by 2)
+                               ;; (format t "i: ~a~%" (diamond-perimeter i))
+                               (summing (diamond-perimeter i))))
+         (hypo-len (floor (1- (* tri-length 2)) 2)))
+    (format t "all-seen-reachable-evens: ~a~%" all-seen-reachable-evens)
+    (format t "all-seen-reachable-odds: ~a~%" all-seen-reachable-odds)
+    (format t "side-to-side: ~a~%" side-to-side)
+    (format t "initial-remaining: ~a~%" initial-remaining)
     (format t "interior-size: ~a~%" interior-size)
-    (* all-seen-reachable interior-size)))
+    (format t "tri-length: ~a~%" tri-length)
+    (format t "hypo-len: ~a~%" hypo-len)
+    (format t "seen-bottom: ~a~%" seen-bottom)
+    (format t "seen-top: ~a~%" seen-top)
+    (format t "seen-left: ~a~%" seen-top)
+    (format t "seen-right: ~a~%" seen-top)
+    (+ (* all-seen-reachable-evens interior-size-odds)
+       (* all-seen-reachable-odds interior-size-evens)
+       seen-bottom
+       seen-left
+       seen-right
+       seen-top
+       (* seen-top-right-edge hypo-len)
+       (* seen-bottom-right-edge hypo-len)
+       (* seen-top-left-edge hypo-len)
+       (* seen-bottom-left-edge hypo-len))))
 
 (defun part-2 (&optional (relative-path-name "src/day-21.in"))
   (bind ((problem (read-problem relative-path-name)))
-    (totals problem 26501365)))
+    (totals problem 26501365 ;; 589 ;; 458 ;; 26501365
+            )))
 
 ;; 130 x 130 grid
 ;; Wrong: 623128465540000
+;; Wrong: 623119228394556
+;; Wrong: 2492501544794756
+;; Wrong: 2492501544768136
+;; Wrong: 623124610114329
+;; Wrong: 623130770523477
+;; Wrong: 623130770550097
+;; Wrong: 620757164217893
+;; Wrong: 621937848921722
+;; Wrong: 621937800167297
+;; Wrong: 621937788464320
 
 (defun test-2 ()
   (part-2 "src/day-21-test.in"))
