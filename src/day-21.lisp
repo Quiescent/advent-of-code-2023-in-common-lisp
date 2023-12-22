e3f6a29e8079243d5dbec1c3ef2a302e4db771a9
Edward John Steere <edward.steere@gmail.com>
2023-12-22 14:27:53 +0200

    Solve day 21

--- src/day-21.lisp
+++ src/day-21.lisp
@@ -111,6 +111,10 @@
     (-> (aref grid (mod y (length grid)))
       (aref (mod x (length (aref grid 0)))))))
 
+(defun meta-coord (c)
+  (complex (floor (realpart c) 131)
+           (floor (imagpart c) 131)))
+
 (defun infini-bfs (grid start max-distance)
   (bind ((queue (empty-seq))
          (seen (make-hash-table :test #'equal))
@@ -135,7 +139,64 @@
           (setf queue (with-last queue next)))))
     (iter
       (for (key value) in-hashtable seen)
-      (counting (= 0 (mod value 2))))))
+      (counting (= 1 (mod value 2)))
+      ;; (when (member (meta-coord key)
+      ;;               (list (complex 0 0)
+      ;;                     (complex 0 1)
+      ;;                     (complex 0 2)
+      ;;                     (complex 0 3)
+      ;;                     (complex 0 -1)
+      ;;                     (complex 0 -2)
+      ;;                     (complex 0 -3)
+      ;;                     (complex 1 0)
+      ;;                     (complex 2 0)
+      ;;                     (complex 3 0)
+      ;;                     (complex -1 0)
+      ;;                     (complex -2 0)
+      ;;                     (complex -3 0)
+      ;;                     (complex 1 1)
+      ;;                     (complex 1 -1)
+      ;;                     (complex -1 1)
+      ;;                     (complex -1 -1)
+
+      ;;                     ;; internal-up-left
+      ;;                     (complex -1 -2)
+      ;;                     (complex -2 -1)
+
+      ;;                     ;; internal-up-right
+      ;;                     (complex 1 -2)
+      ;;                     (complex 2 -1)
+
+      ;;                     ;; internal-down-left
+      ;;                     (complex -1 2)
+      ;;                     (complex -2 1)
+
+      ;;                     ;; internal-down-right
+      ;;                     (complex 1 2)
+      ;;                     (complex 2 1)
+                          
+      ;;                     ;; external-up-left
+      ;;                     (complex -1 -3)
+      ;;                     (complex -2 -2)
+      ;;                     (complex -3 -1)
+
+      ;;                     ;; externals-up-right
+      ;;                     (complex 1 -3)
+      ;;                     (complex 2 -2)
+      ;;                     (complex 3 -1)
+
+      ;;                     ;; externals-down-left
+      ;;                     (complex -1 3)
+      ;;                     (complex -2 2)
+      ;;                     (complex -3 1)
+
+      ;;                     ;; externals-down-right
+      ;;                     (complex 1 3)
+      ;;                     (complex 2 2)
+      ;;                     (complex 3 1)
+      ;;                     ))
+      ;;  (counting (= 1 (mod value 2))))
+      )))
 
 (defun count-reachable (seen)
   (iter
@@ -220,17 +281,24 @@
                                  #'count-reachable-odds
                                  #'count-reachable))
          (seen-top-right-edge (funcall from-side-function
-                                       (merge-seen seen-entering-bottom
-                                                   seen-entering-left)))
+                                       (unlocked-bfs grid
+                                                     (complex (1- (length grid))
+                                                              0)
+                                                     195)))
          (seen-bottom-right-edge (funcall from-side-function
-                                          (merge-seen seen-entering-top
-                                                      seen-entering-left)))
+                                          (unlocked-bfs grid
+                                                        (complex (1- (1- (length grid)))
+                                                                 (1- (length grid)))
+                                                        195)))
          (seen-top-left-edge (funcall from-side-function
-                                      (merge-seen seen-entering-right
-                                                  seen-entering-bottom)))
+                                      (unlocked-bfs grid
+                                                    (complex 0 0)
+                                                    195)))
          (seen-bottom-left-edge (funcall from-side-function
-                                         (merge-seen seen-entering-right
-                                                     seen-entering-top)))
+                                         (unlocked-bfs grid
+                                                       (complex 0
+                                                                (1- (length grid)))
+                                                       195)))
          (all-seen-reachable-evens (count-reachable start-bfs))
          (all-seen-reachable-odds (count-reachable-odds start-bfs))
          (side-to-side (1+ (* 2 (1- tri-length))))
@@ -268,6 +336,99 @@
        (* seen-from-bottom-left tri-length)
        (* seen-from-bottom-right tri-length))))
 
+(defun explored-dark (distances)
+  (iter
+    (for (key value) in-hashtable distances)
+    (counting (= (mod value 2) 1))))
+
+(defun explored-light (distances)
+  (iter
+    (for (key value) in-hashtable distances)
+    (counting (= (mod value 2) 0))))
+
+(defun total-reachable (grid max-steps)
+  (bind ((grids-to-side (floor max-steps 131))
+         (distances (unlocked-bfs grid
+                                  (complex 65 65)
+                                  max-steps))
+         (dark-count (explored-dark distances))
+         (light-count (explored-light distances))
+         (dark-grid-count (iter
+                            (for i from 1 to grids-to-side by 2)
+                            (summing (diamond-perimeter i))))
+         (light-grid-count (iter
+                             (for i from 2 to grids-to-side by 2)
+                             (summing (diamond-perimeter i))))
+         (main-axis-counter (if (= (mod grids-to-side 2) 1)
+                                #'explored-dark
+                                #'explored-light))
+         (left-count (funcall main-axis-counter
+                              (unlocked-bfs grid
+                                            (complex 130 65)
+                                            130)))
+         (right-count (funcall main-axis-counter
+                               (unlocked-bfs grid
+                                             (complex 0 65)
+                                             130)))
+         (up-count (funcall main-axis-counter
+                            (unlocked-bfs grid
+                                          (complex 65 0)
+                                          130)))
+         (down-count (funcall main-axis-counter
+                              (unlocked-bfs grid
+                                            (complex 65 130)
+                                            130)))
+         (internal-axis-counter (if (= (mod grids-to-side 2) 1)
+                                    #'explored-light
+                                    #'explored-dark))
+         (internal-count (1- grids-to-side))
+         (internal-up-left (funcall internal-axis-counter
+                                    (unlocked-bfs grid
+                                                  (complex 130 130)
+                                                  195)))
+         (internal-up-right (funcall internal-axis-counter
+                                     (unlocked-bfs grid
+                                                   (complex 0 130)
+                                                   195)))
+         (internal-down-left (funcall internal-axis-counter
+                                      (unlocked-bfs grid
+                                                    (complex 130 0)
+                                                    195)))
+         (internal-down-right (funcall internal-axis-counter
+                                       (unlocked-bfs grid
+                                                     (complex 0 0)
+                                                     195)))
+         (external-up-left (funcall main-axis-counter
+                                    (unlocked-bfs grid
+                                                  (complex 130 130)
+                                                  64)))
+         (external-up-right (funcall main-axis-counter
+                                     (unlocked-bfs grid
+                                                   (complex 0 130)
+                                                   64)))
+         (external-down-left (funcall main-axis-counter
+                                      (unlocked-bfs grid
+                                                    (complex 130 0)
+                                                    64)))
+         (external-down-right (funcall main-axis-counter
+                                       (unlocked-bfs grid
+                                                     (complex 0 0)
+                                                     64))))
+    (+ (* dark-count dark-grid-count)
+       (* light-count light-grid-count)
+       left-count
+       right-count
+       up-count
+       down-count
+       (* internal-count internal-up-left)
+       (* internal-count internal-up-right)
+       (* internal-count internal-down-left)
+       (* internal-count internal-down-right)
+       (* external-up-left grids-to-side)
+       (* external-up-right grids-to-side)
+       (* external-down-left grids-to-side)
+       (* external-down-right grids-to-side))))
+
 (defun grid-distances (grid)
   (bind ((start (starting-point grid)))
     (iter
@@ -283,9 +444,11 @@
   (bind ((problem (read-problem relative-path-name)))
     ;; (totals problem 26501365 ;; 589 ;; 458 ;; 26501365
     ;;         )
-    (format t "Result: ~a~%" (totals problem 458 ;; 26501365
+    ;; (format t "Infini: ~a~%" (infini-bfs problem (complex 65 65) 458))
+    (format t "Result: ~a~%" (total-reachable problem 26501365 ;; 458 ;; 26501365
                                      ))
-    (grid-distances problem)))
+    ;; (grid-distances problem)
+    ))
 
 ;; 130 x 130 grid
 ;; Wrong: 623128465540000
